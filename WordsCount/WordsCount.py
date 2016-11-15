90e87a4f07084ad2af057bf7d83e026e9c50d6cd
robpqt <robpqt@MacBook-Pro-de-Robin-2.local>
2016-11-15 16:02:52 -0500

    Nouveau Repartition temps de parole sans modérateur

--- WordsCount/WordsCount.py
+++ WordsCount/WordsCount.py
@@ -1,11 +1,6 @@
 import csv
 
-def filter(link):
-
-	#Création des fichiers qui contiendront les mots de chaques participants
-	#fout1 = open("/Users/robpqt/Documents/leDevoir/data/sortie_filter_1.txt","w")
-	#fout2 = open("/Users/robpqt/Documents/leDevoir/data/sortie_filter_2.txt","w")
-	#fout3 = open("/Users/robpqt/Documents/leDevoir/data/sortie_filter_3.txt","w")
+def faireTableau(link):
 
 	content = []
 	content1 = []
@@ -21,7 +16,6 @@ def filter(link):
 				content.append(word)
 				i+=1
 	
-
 	for idx,item in enumerate(content):
 
 		#Gestion du 1er Cas
@@ -42,8 +36,29 @@ def filter(link):
 				k+=1
 			res2=k-idx
 
-			#Test de celui qui parlera en prochain
-			res=min(res1,res2)
+			k=idx+1
+			while content[k]!="RADDATZ:":
+				if k==len(content)-1:
+					break
+				k+=1
+			res3=k-idx
+
+			k=idx+1
+			while content[k]!="COOPER:":
+				if k==len(content)-1:
+					break
+				k+=1
+			res4=k-idx
+
+			k=idx+1
+			while content[k]!="WALLACE:":
+				if k==len(content)-1:
+					break
+				k+=1
+			res5=k-idx
+
+			#faireCsv de celui qui parlera en prochain
+			res=min(res1,res2,res3,res4,res5)
 
 			for i in range(idx+1,res+idx):
 				content1.append(content[i])
@@ -58,14 +73,35 @@ def filter(link):
 				j+=1
 			res1=j-idx
 			
-			k=idx+1	
+			k=idx+1
 			while content[k]!="HOLT:":
 				if k==len(content)-1:
 					break
 				k+=1
 			res2=k-idx
 
-			res=min(res1,res2)
+			k=idx+1
+			while content[k]!="RADDATZ:":
+				if k==len(content)-1:
+					break
+				k+=1
+			res3=k-idx
+
+			k=idx+1
+			while content[k]!="COOPER:":
+				if k==len(content)-1:
+					break
+				k+=1
+			res4=k-idx
+
+			k=idx+1
+			while content[k]!="WALLACE:":
+				if k==len(content)-1:
+					break
+				k+=1
+			res5=k-idx
+
+			res=min(res1,res2,res3,res4,res5)
 
 			for i in range(idx+1,res+idx):
 				content2.append(content[i])
@@ -92,40 +128,97 @@ def filter(link):
 			for i in range(idx+1,res+idx):
 				content3.append(content[i])
 
-	#Ecriture dans les 3 fichers
-	#fout1.write(str(content1))	
-	#fout2.write(str(content2))					
-	#fout3.write(str(content3))	
-	#fout1.close()
-	#fout2.close()
-	#fout3.close()
+		if item=="RADDATZ:":
+		
+			j=idx+1
+			while content[j]!="CLINTON:":
+				if j==len(content)-1:
+					break
+				j+=1
+			res1=j-idx
+		
+			k=idx+1			
+			while content[k]!="TRUMP:":
+				if k==len(content)-1:
+					break
+				k+=1
+			res2=k-idx
+
+			res=min(res1,res2)
+
+			for i in range(idx+1,res+idx):
+				content3.append(content[i])
+
+		if item=="COOPER:":
+		
+			j=idx+1
+			while content[j]!="CLINTON:":
+				if j==len(content)-1:
+					break
+				j+=1
+			res1=j-idx
+		
+			k=idx+1			
+			while content[k]!="TRUMP:":
+				if k==len(content)-1:
+					break
+				k+=1
+			res2=k-idx
+
+			res=min(res1,res2)
+
+			for i in range(idx+1,res+idx):
+				content3.append(content[i])
+
+		if item=="WALLACE:":
+		
+			j=idx+1
+			while content[j]!="CLINTON:":
+				if j==len(content)-1:
+					break
+				j+=1
+			res1=j-idx
+		
+			k=idx+1			
+			while content[k]!="TRUMP:":
+				if k==len(content)-1:
+					break
+				k+=1
+			res2=k-idx
+
+			res=min(res1,res2)
+
+			for i in range(idx+1,res+idx):
+				content3.append(content[i])
 
 	results = []
 	results.append(content1)
 	results.append(content2)
-	results.append(content3)
+	#results.append(content3)
 
-	print("Le comptage de mot du débat 1 a marché!")
+	print("Mise en Tableau OK")
 
 	return results
 
-def test():
-	foutWordsCount = open("data/WordsCount.csv","w")
+def faireCsv(linkin, linkout):
+	foutWordsCount = open(linkout,"w")
 	c = csv.writer(foutWordsCount)
 
-	wordsTable = filter("data/textToAnalyze.txt")
+	wordsTable = faireTableau(linkin)
 	wordsCount = []
 
 	i=0
-	for i in range(0,3):	
+	for i in range(0,2):	
 		wordsCount.append(len(wordsTable[i]))
 
 	c.writerow(["person","wordsCount"])
 	c.writerow(["Clinton",wordsCount[0]])
 	c.writerow(["Trump",wordsCount[1]])
-	c.writerow(["Moderator",wordsCount[2]])
+	#c.writerow(["Moderator",wordsCount[2]])
 
-	print("L'écriture du CSV du débat 1 a marché!")
+	print("Mise en CSV OK")
 
-test()
+faireCsv("data/textToAnalyze.txt", "data/WordsCount.csv")
+faireCsv("data/textToAnalyze2.txt","data/WordsCount2.csv")
+faireCsv("data/textToAnalyze3.txt","data/WordsCount3.csv")
 
--- WordsCount/main.sh
+++ WordsCount/main.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 python3 WordsCount.py
-python3 WordsCount2.py
-python3 WordsCount3.py
+
